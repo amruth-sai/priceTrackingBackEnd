f7bb451337bb4bcf47a23e86b55c9a6a05a1af66
Amruth Sai Poreddy <amruth.poreddy@techigai.io>
2022-05-10 11:25:44 +0530

    basics changes

--- addProduct.js
+++ addProduct.js
@@ -7,55 +7,62 @@ var url =
   "mongodb+srv://admin:admin@producttracking.7oyuk.mongodb.net/admin1?retryWrites=true&w=majority";
 
 router.post("/", function (req, response) {
-  var status=true;
+  var status = true;
   console.log(req.body);
   var link1 = req.body.link;
   var username = req.body.username;
   var cost;
   var iLink;
   console.log(link1, username);
-  try{
-  request(link1, (err, res, html) => {
-    // console.log(html);
-    var $ = cheerio.load(html);
-    console.log("Here");
-    var name="";
-    try{
-    cost = $(".apexPriceToPay").text().split("₹")[1]; //PriceToPay
-    iLink = $("#imgTagWrapperId").children().first()["0"]["attribs"]["src"];
-    name = $("#productTitle").text() //PriceToPay
-    }
-    catch(err){
-      console.log("ERROR HERE");
-      status=false;
-      response.send({success:false})
-      return;
-    }
-    console.log(cost.split("₹")[1]);
-    console.log(iLink);
-    MongoClient.connect(
-      url,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      async function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("admin1");
-        const result=await dbo.collection("users").findOne({username: username});
-
-        var myobj = { link: link1, price: cost, mail: result["mail"], imgLink: iLink,username: username,name:name};
-
-        console.log(cost);
-        dbo.collection("maincollection").insertOne(myobj, (err, res) => {
-          if (err) throw err;
-          console.log("INSERTED");
-          db.close();
-        });
-        response.send({success:true})
+  try {
+    request(link1, (err, res, html) => {
+      // console.log(html);
+      var $ = cheerio.load(html);
+      console.log("Here");
+      var name = "";
+      try {
+        cost = $(".apexPriceToPay").text().split("₹")[1]; //PriceToPay
+        iLink = $("#imgTagWrapperId").children().first()["0"]["attribs"]["src"];
+        name = $("#productTitle").text(); //PriceToPay
+      } catch (err) {
+        console.log("ERROR HERE");
+        status = false;
+        response.send({ success: false });
+        return;
       }
-    );
-  })}
-  catch(err) {
-    status=false; 
+      console.log(cost.split("₹")[1]);
+      console.log(iLink);
+      MongoClient.connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        async function (err, db) {
+          if (err) throw err;
+          var dbo = db.db("admin1");
+          const result = await dbo
+            .collection("users")
+            .findOne({ username: username });
+
+          var myobj = {
+            link: link1,
+            price: cost,
+            mail: result["mail"],
+            imgLink: iLink,
+            username: username,
+            name: name,
+          };
+
+          console.log(cost);
+          dbo.collection("maincollection").insertOne(myobj, (err, res) => {
+            if (err) throw err;
+            console.log("INSERTED");
+            db.close();
+          });
+          response.send({ success: true });
+        }
+      );
+    });
+  } catch (err) {
+    status = false;
   }
-  
 });
 module.exports = router;
--- findChange.js
+++ findChange.js
@@ -1,87 +1,74 @@
-var request= require("request");
+var request = require("request");
 var cheerio = require("cheerio");
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'cvrita2018@gmail.com',
-      pass: ''
-    }
-  });  
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://admin:admin@producttracking.7oyuk.mongodb.net/admin1?retryWrites=true&w=majority";
-
-MongoClient.connect(url, { useNewUrlParser: true ,useUnifiedTopology: true},function(err, db) {
-    if (err)
-        throw err;
-    var dbo = db.db("admin1");
-  
-    var cursor = dbo.collection('maincollection').find();
-
-    cursor.each(function(err, item) {
-        if(item == null) {
-           db.close(); 
-            return;
-        }
-        let res =  async function isChange(link,price){
-
-            var cost;
-            return request(link,async function(err,response,html){
-                var $ = cheerio.load(html);
-                $("#priceblock_ourprice").filter(function(){
-                    var data=$(this);
-                    cost=data.text();
-                    //console.log(cost);
-                    
-                })
-                $("#priceblock_dealprice").filter(function(){
-                    var data=$(this);
-                    cost=data.text();
-                    //console.log(cost);
-                })
-                await cost;
-                if(cost===price){
-                    // console.log(cost+" "+price);
-                    // var mailOptions = {
-                    //     from: 'cvrita2018@gmail.com',
-                    //     to: item.mail,
-                    //     subject: 'PRICE DROP ALERT',
-                    //     text: 'Hurry! price drop on '+item.link
-                    //   };
-                      
-                    //   transporter.sendMail(mailOptions, function(error, info){
-                    //     if (error) {
-                    //       console.log(error);
-                    //     } else {
-                    //       console.log('Email sent: ' + info.response);
-                    //     }
-                    //   });
-                    // return ;
-                }
-                else{
-                    var mailOptions = {
-                        from: 'cvrita2018@gmail.com',
-                        to: item.mail,
-                        subject: 'PRICE DROP ALERT',
-                        text: 'Hurry! price drop on '+item.link
-                      };
-                      
-                      transporter.sendMail(mailOptions, function(error, info){
-                        if (error) {
-                          console.log(error);
-                        } else {
-                          console.log('Email sent: ' + info.response);
-                        }
-                      });
-                    return ;
-                }
-                
-            });
-        
-        }
-        res(item.link,item.price);
-    });
-   
+  service: "gmail",
+  auth: {
+    user: "cvrita2018@gmail.com",
+    pass: "",
+  },
 });
+
+var MongoClient = require("mongodb").MongoClient;
+var url =
+  "mongodb+srv://admin:admin@producttracking.7oyuk.mongodb.net/admin1?retryWrites=true&w=majority";
+var name = "";
+var cost = "";
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  async function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("admin1");
+
+    var cursor = await dbo.collection("maincollection").find();
+
+    cursor.each(async function (err, item) {
+      if (item == null) {
+        db.close();
+        return;
+      }
+      console.log(item);
+      // console.log("nbhkbvdsbj")
+      var temp = item.link;
+      var price = item.price;
+      console.log(temp, price);
+      await check(temp,price);
+      
+    });
+  }
+);
+async function check(temp, price) {
+  console.log("Here");
+  request(temp, (err, res, html) => {
+    console.log(html);
+    var $ = cheerio.load(html);
+    console.log("Here");
+    var name = "";
+
+    cost = $(".apexPriceToPay").text().split("₹")[1]; //PriceToPay
+    // iLink = $("#imgTagWrapperId").children().first()["0"]["attribs"]["src"];
+    name = $("#productTitle").text(); //PriceToPay
+
+    console.log(cost.split("₹")[1]);
+    console.log(iLink);
+    if (cost.split("₹")[1] !== price) {
+      var mailOptions = {
+        from: "cvrita2018@gmail.com",
+        to: item.mail,
+        subject: "PRICE DROP ALERT",
+        text: "Hurry! price drop on " + item.link,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      return;
+    }
+  });
+}
